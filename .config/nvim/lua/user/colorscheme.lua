a14c8f36fdf5008cdb7ba10c3932683664b816a3
Jom Dollesin <gitnightjd@gmail.com>
2022-04-23 11:30:59 +0800

    Set colorscheme to gruvbox.

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -2,7 +2,7 @@ vim.cmd [[
 try
   syntax enable
   set background=dark
-  colorscheme nord
+  colorscheme gruvbox
   " let g:neosolorized_termtrans=1
   " runtime ../../colors/NeoSolarized.vim
   " colorscheme NeoSolarized
