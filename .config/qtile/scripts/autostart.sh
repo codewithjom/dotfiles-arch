3a7d7ab80fcd2cc6511955baacbe1f13b490f5ec
Jom Dollesin <codewithjom@gmail.com>
2022-07-31 08:54:33 +0800

    Adding programs for autostart.

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+polybar -q bar &
+lxsession &
+picom --experimental-backends -b &
+nm-applet &
+volumeicon &
+xsetroot -cursor_name left_ptr &
+pamac-tray &
+blueberry-tray &
 sxhkd -c ~/.config/qtile/scripts/sxhkdrc &
 nitrogen --set-scaled --restore &
