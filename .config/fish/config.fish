3c71b2870bc0dcbf372060f986f3bdbb7c615003
Jom Dollesin <codewithjom@gmail.com>
2022-08-24 15:50:34 +0800

    new(fish): adding new alias.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -43,6 +43,7 @@ alias zathura 'devour zathura'
 # alias clear 'clear && colorscript -e 13'
 alias checkupdates 'checkupdates | less'
 alias . 'tmux new-session -s 1'
+alias how-old-am-i "sudo dumpe2fs $(mount | grep 'on \/ ' | awk '{print $1}') | grep 'Filesystem created:'"
 
 if status --is-login
   if test -z "$DISPLAY" -a $XDG_VTNR = 1
