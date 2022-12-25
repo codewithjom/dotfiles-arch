b5049f75e70887bb0f6ba0e3807fc44b7e6bf99e
Jom Dollesin <codewithjom@gmail.com>
2022-12-25 16:11:26 +0800

    feat(qtile): added emacs daemon.

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -12,3 +12,4 @@ pamac-tray &
 sxhkd -c ~/.config/qtile/scripts/sxhkdrc &
 nitrogen --set-scaled --restore &
 xidlehook --not-when-audio --not-when-fullscreen --timer 600 'systemctl suspend' '' &
+/usr/bin/emacs --daemon &
