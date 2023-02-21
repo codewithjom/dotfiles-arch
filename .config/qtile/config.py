abec3de8de92027b943ecd6b9776f5921cc0b3fd
Jom Dollesin <codewithjom@gmail.com>
2023-02-21 09:29:27 +0800

    fix(qtile): changing the height && width in scratchpad.

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -97,8 +97,8 @@ layouts = [
 ]
 
 groups.append(ScratchPad('scratchpad', [
-    DropDown('term', terminal, width=0.6, height=0.7, x=0.2, y=0.2),
-    DropDown('em', emacs, width=0.5, height=0.8, x=0.3, y=0.2),
+    DropDown('term', terminal, width=0.8, height=0.7, x=0.1, y=0.2),
+    DropDown('em', emacs, width=0.8, height=0.7, x=0.1, y=0.2),
 ]))
 keys.extend([
     Key([mod], "w", lazy.group['scratchpad'].dropdown_toggle('term')),
