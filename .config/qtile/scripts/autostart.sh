0725a898002ccaa9ffa033611c39ef5bfc738b72
Jom Dollesin <codewithjom@gmail.com>
2023-03-22 21:59:10 +0800

    feat: minor updates.

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -11,5 +11,5 @@ pamac-tray &
 # blueberry-tray &
 sxhkd -c ~/.config/qtile/scripts/sxhkdrc &
 nitrogen --set-scaled --restore &
-xidlehook --not-when-audio --not-when-fullscreen --timer 1800 'systemctl suspend;i3lock -c 000000' '' &
+xidlehook --not-when-audio --not-when-fullscreen --timer 900 'systemctl suspend;i3lock -c 000000' '' &
 # /usr/bin/emacs --daemon &
