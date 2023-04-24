3414bacaa87373514ae8dd82a52a6dab55ddca34
Jom Dollesin <codewithjom@gmail.com>
2023-04-25 00:13:05 +0800

    fix(i3): autostart.sh deleted.

--- .config/i3/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-lxsession &
-picom -b &
-nm-applet &
-xsetroot -cursor_name left_ptr &
-sxhkd -c ~/.config/i3/sxhkdrc &
-nitrogen --set-zoom-fill $HOME/Downloads/wallpapers/0383.png &
-xidlehook --not-when-audio --not-when-fullscreen --timer 900 'systemctl suspend;i3lock -c 000000' '' &
